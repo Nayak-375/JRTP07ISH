e9e16eaccc4c08895951305e881a940cb5b8aeb1
Aditya Nayak <nayakanha375@gmail.com>
2026-05-17 11:56:55 +0530

    Verified Print Citizen Deleted

--- src/main/java/com/nt/controle/SSAWebOperationRestController.java
+++ src/main/java/com/nt/controle/SSAWebOperationRestController.java
@@ -13,7 +13,7 @@ public class SSAWebOperationRestController {
     @GetMapping("//find/{ssn}")
 	public ResponseEntity<?>getStateBySSN(@PathVariable Integer ssn)
 	{
-    	System.out.println("Verified Citizen");
+    	
     	System.out.println(" new branch  created");
     	
 		if(String.valueOf(ssn).length()!=9)
